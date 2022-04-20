2ef8995f389a90aec590d3b9b50ca87457cd644f
Fabio Murillo <fabiomurillo2020@outlook.com>
2022-04-20 15:27:23 -0500

    agregando pruebas unitarias

--- Test/missionCommander.test.js
+++ Test/missionCommander.test.js
@@ -1,9 +1,22 @@
-const MissionCommander = require('./../app/missionCommander');
+const MissionCommander = require('../App/missionCommander');
 
 describe("Unit Tests for Mission Commander class", () => {
     test('1) Create a mision commander objet', () => {
-      const result = 5 + 5 
-      expect(result).toBe(10);
+      const Carlo = new MissionCommander("Carlo")
+      expect(Carlo.name).toBe("Carlo");
     });
   })
-  
+
+describe("Unit Tests for Mission Commander class", () => {
+    test('2) Create a second mision commander objet', () => {
+      const Fernanda = new MissionCommander("Fernanda")
+      expect(Fernanda.name).toBe("Fernanda");
+    });
+  })
+
+describe("Unit Tests for Mission Commander class", () => {
+    test('3) Create a third mision commander objet', () => {
+      const Rodrigo = new MissionCommander("Rodrigo")
+      expect(Rodrigo.name).toBe("Rodrigo");
+    });
+  })
