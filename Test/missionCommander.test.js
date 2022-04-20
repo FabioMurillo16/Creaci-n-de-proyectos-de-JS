fcaf7576fb7770b14f0586007e37bcfe787eb954
Fabio Murillo <fabiomurillo2020@outlook.com>
2022-04-20 15:00:34 -0500

    Agregando descripción a la prueba unitaria

---
+++ Test/missionCommander.test.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./../app/missionCommander');
+
+describe("Unit Tests for Mission Commander class", () => {
+    test('1) Create a mision commander objet', () => {
+      const result = 5 + 5 
+      expect(result).toBe(10);
+    });
+  })
+  
